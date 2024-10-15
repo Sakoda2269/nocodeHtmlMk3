c03241abe89aaa8a5480a05210ab8dd646a84533
Sakoda2269 <Ksakoda.2269@gmail.com>
2024-10-15 21:09:53 +0900

    レイヤーボタンを追加、ヘッダーを追加

--- app/projects/[pid]/page.js
+++ app/projects/[pid]/page.js
@@ -2,7 +2,7 @@
 import Canvas from "@/components/canvas/canvas";
 import styles from "./page.module.css"
 import { useState } from "react";
-import { FaPlus, FaDatabase } from "react-icons/fa6";
+import { FaPlus, FaDatabase, FaLayerGroup  } from "react-icons/fa6";
 
 export default function Project({params}) {
     
@@ -11,6 +11,11 @@ export default function Project({params}) {
     const leftClass = {
         true: `${styles.container} ${styles.left}`,
         false: `${styles.container} ${styles.leftClose}`
+    }
+
+    const buttonClass = {
+        true: "btn btn-secondary",
+        false: "btn border-secondary"
     }
     
     const openMenubar = (num) => {
@@ -23,31 +28,51 @@ export default function Project({params}) {
     
     return (
         <div>
+            <div className={`${styles.header}`}>
+            </div>
             <div className={`${styles.all}`}>
                 <div className={`${styles.leftIcons}`}>
                     <div>
-                        <button className="btn border-secondary" onClick={() => {openMenubar(1)}}>
+                        <button className={buttonClass[open == 1]} onClick={() => {openMenubar(1)}}>
+                            <FaLayerGroup />
+                        </button>
+                    </div>
+                    <div>
+                        <button className={buttonClass[open == 2]} onClick={() => {openMenubar(2)}}>
                             <FaPlus />
                         </button>
                     </div>
                     <div>
-                        <button className="btn border-secondary" onClick={() => {openMenubar(2)}}>
+                        <button className={buttonClass[open == 3]} onClick={() => {openMenubar(3)}}>
                             <FaDatabase />
                         </button>
                     </div>
                 </div>
-                <div className={`${leftClass[open==1]}`}>
-                    <span style={{width: "80%"}}>
-                        <button style={{width: "100%"}}>hello</button>
-                    </span>
-                    <span style={{width: "80%"}}>
-                        <button style={{width: "100%"}}>hello</button>
-                    </span>
-                    <span style={{width: "80%"}}>
-                        <button style={{width: "100%"}}>hello</button>
-                    </span>
+                <div className={`${leftClass[open!=0]}`}>
+                    {open == 1 && 
+                        <div className={`${styles.leftContainer}`}>
+
+                        </div>
+                    }
+                    {open == 2 && 
+                        <div className={`${styles.leftContainer}`}>
+                            <span style={{width: "80%"}}>
+                                <button style={{width: "100%"}}>hello</button>
+                            </span>
+                            <span style={{width: "80%"}}>
+                                <button style={{width: "100%"}}>hello</button>
+                            </span>
+                            <span style={{width: "80%"}}>
+                                <button style={{width: "100%"}}>hello</button>
+                            </span>
+                        </div>
+                    } 
+                    {open == 3 && 
+                        <div className={`${styles.leftContainer}`}>
+                            good
+                        </div>
+                    } 
                 </div>
-                <div className={`${leftClass[open==2]}`}>good</div>
 
                 <div className={`${styles.container} ${styles.center}`}>
                     <Canvas />
